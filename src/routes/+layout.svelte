abe6b44b4c4c8aa7c2fc08c6e29d480015a57382
shawn edgell <shawn_edgell@hotmail.com>
2024-03-06 06:03:54 -0500

    undo scroll to top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
+	import '../app.postcss';
 	import Navigation from '$lib/components/Navigation.svelte';
 
-	// Explicitly declare the type of scrollContainer as HTMLDivElement | null
-	let scrollContainer: HTMLDivElement | null = null;
-
+	// Scroll to top whenever the page changes
 	onMount(() => {
-		// Subscribe to page store changes
 		page.subscribe(() => {
-			// Check if the scrollContainer is available and scroll to top
-			if (scrollContainer) {
-				scrollContainer.scrollTo(0, 0);
-			}
+			window.scrollTo(0, 0);
 		});
 	});
 </script>
 
 <div class="flex flex-col h-screen justify-end sm:justify-between">
+	<!-- Conditional rendering for Navigation based on screen size -->
 	<div class="sm:block hidden">
 		<Navigation />
 	</div>
-	<!-- Bind the scrollable div to the scrollContainer variable -->
-	<div bind:this={scrollContainer} class="flex-1 scroll-smooth overflow-y-auto space-y-12 p-4">
+	<!-- Scrollable Router Slot -->
+	<div class="flex-1 overflow-y-auto space-y-12 p-4">
 		<slot />
 	</div>
+	<!-- Navigation for larger screens, if needed at the top -->
 	<div class="sm:hidden">
 		<Navigation />
 	</div>
