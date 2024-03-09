7d3680dbecd9a49cf93c39c3c1bb123795fae220
shawn edgell <shawn_edgell@hotmail.com>
2024-03-09 17:21:11 -0500

    fixed styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,16 @@
 </script>
 
 <!-- Your layout component -->
-<div class="h-full flex flex-col">
+<div class="h-full flex flex-col items-center">
 	<!-- Top Navigation - Visible on medium and larger screens -->
 	<div class="hidden sm:block sticky top-0 w-screen z-10">
 		<Navigation />
 	</div>
 
 	<!-- Main Content Area - Allows for independent scrolling -->
-	<div class="flex-1 flex justify-center">
-		<div class="max-w-4xl w-full p-4">
-			<slot />
-		</div>
+
+	<div class="flex-1 max-w-4xl w-full p-4">
+		<slot />
 	</div>
 
 	<!-- Bottom Navigation - Visible on small screens -->
